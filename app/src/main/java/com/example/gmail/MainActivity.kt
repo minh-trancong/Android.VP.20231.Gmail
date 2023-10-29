665de5e721e78c4fcc47a1bb82f04446cb735287
Tran Cong Minh <tc.tranminh@gmail.com>
2023-10-29 23:44:00 +0700

    feat: new gmail UI ✨

--- app/src/main/java/com/example/gmail/MainActivity.kt
+++ app/src/main/java/com/example/gmail/MainActivity.kt
@@ -1,46 +1,43 @@
 package com.example.gmail
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.gmail.ui.theme.GmailTheme
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+import java.time.LocalTime
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            GmailTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
+
+        val mailItems = ArrayList(listOf(
+            Mail_item(
+                title = "Meeting Reminder",
+                context = "Don't forget about the team meeting tomorrow at 10am.",
+                receiver = "John Doe",
+                LocalTime.now(),
+                resources.getIdentifier("avatar", "drawable", packageName)
+            ),
+            Mail_item(
+                title = "Flight Confirmation",
+                context = "Your flight to San Francisco on December 15th is confirmed.",
+                receiver = "Jane Smith",
+                LocalTime.now(),
+                resources.getIdentifier("avatar", "drawable", packageName)
+            ),
+            Mail_item(
+                title = "Dinner Reservation",
+                context = "Your dinner reservation for 2 at La Petite Maison is confirmed for 7pm tonight.",
+                receiver = "Alice Johnson",
+                LocalTime.now(),
+                resources.getIdentifier("avatar", "drawable", packageName)
+            )
+        ))
+
+        findViewById<ListView>(R.id.MailContent).adapter = GmailAdapter(mailItems)
+
+
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GmailTheme {
-        Greeting("Android")
-    }
-}
